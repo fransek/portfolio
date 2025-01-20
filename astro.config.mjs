06b0555a0f43b3415dab914dae8a499fc2e6af20
Frans Ekman <fransedvinekman@gmail.com>
2025-01-20 20:30:08 +0100

    image fix and path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
-
 import vercel from '@astrojs/vercel'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel({
+    imageService: true,
     isr: {
       expiration: 60 * 60,
     },
---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const projects = [
+  'fransek/statekit',
+  'fransek/next-i18n',
+  'fransek/package-template',
+] as const
